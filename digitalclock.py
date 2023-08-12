d53d8e5731119d860263f904d10484b467dd863e
Krystof Kubica <kkubica912@gmail.com>
2023-08-12 11:18:20 +0200

    comments

--- digitalclock.py
+++ digitalclock.py
@@ -7,15 +7,18 @@ import sys
 master = Tk()
 master.title("DigitalClock")
 
+# Define function to get time in correct format and refresh clocks every 200ms
 def get_time():
     timeVar = time.strftime("%I:%M:%S")
     clock.config(text=timeVar)
     clock.after(200,get_time)
 
-
+# Create clock object with font and colors
 clock = Label(master, font=("Arial",90),bg="black",fg="white")
 clock.pack()
 
+# Call get_time
 get_time()
 
+# Start UI
 master.mainloop()
